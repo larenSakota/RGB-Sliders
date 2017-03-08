35701a9dd7476cecf60c9c88e4d182377ed42bf3
larenSakota <jedi.temple@yahoo.com>
2017-03-07 21:41:55 -0800

    Hi there, i added text fields and updated them for the colors

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -13,18 +13,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var viewColor: UIView!
+    @IBOutlet weak var redValue: UITextField!
+    @IBOutlet weak var greenValue: UITextField!
+    @IBOutlet weak var blueValue: UITextField!
+    
+    @IBAction func redValueChange() {
+        let red = redValue.text
+        let green = greenValue.text
+        let blue = blueValue.text
+        
+        redSlider.value = Float(red!)!
+        greenSlider.value = Float(green!)!
+        blueSlider.value = Float(blue!)!
+        
+        updateBackgroundColor()
+    }
 
     @IBAction func updateBackgroundColor() {
         let red = CGFloat(redSlider.value)
         let green = CGFloat(greenSlider.value)
         let blue = CGFloat(blueSlider.value)
         
-        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        viewColor.backgroundColor = UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+        
+        redValue.text = "\(Int(red))"
+        greenValue.text = "\(Int(green))"
+        blueValue.text = "\(Int(blue))"
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         updateBackgroundColor()
+        redValueChange()
     }
 
     override func didReceiveMemoryWarning() {
